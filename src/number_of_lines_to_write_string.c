880205825f31569d3f6d104b6356e0786d4720bc
patl <patl@vmware.com>
2018-11-25 13:36:39 +0800

    update

---
+++ src/number_of_lines_to_write_string.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  number_of_lines_to_write_string.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/24/2018 10:27:28 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  xueyuanl
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdlib.h>
+
+int* numberOfLines(int* widths, int widthsSize, char* S, int* returnSize) {
+	    
+}
+
