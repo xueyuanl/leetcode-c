16f3b4f68780674df9d9e68ca3e011193b30d8ce
imac_vbox <xueyuanl@imac.com>
2018-11-25 13:49:14 +0800

    update

--- src/number_of_lines_to_write_string.c
+++ src/number_of_lines_to_write_string.c
@@ -19,6 +19,25 @@
 #include <stdlib.h>
 
 int* numberOfLines(int* widths, int widthsSize, char* S, int* returnSize) {
-	    
+    int count_line = 0;
+    int line_number = 0;
+ 	
+    for (int i = 0; *(S + i) != '\0'; i ++) {
+        int temp = *(widths +  (*(S + i) - 'a'));
+        count_line += temp;
+        if (count_line > 100) {
+            line_number ++;
+            count_line = 0;
+            i--;
+        }
+    }
+ 
+    *returnSize = 2;  //the returnSize is simple as a pointer to int, set it to 2 telling we return a size of 2 int array
+    int* res = (int*) malloc(2 * sizeof(int));
+    *res = line_number + 1;
+    *(res + 1) = count_line;
+  
+    return res;
+ 
 }
 
