00d983d6b92d0c583c69424bccd3d8252cd6d8d1
arch.linux <arch.linux@virtualbox.com>
2019-05-06 15:42:49 +0000

    update

--- src/sort_an_array.c
+++ src/sort_an_array.c
@@ -4,41 +4,37 @@
 
 #include <stdlib.h>
 
-//void swap(int *a, int *b) {
-//    int temp = *a;
-//    *a = *b;
-//    *b = temp;
-//}
-//
-//void recursion(int *nums, int numsSize) {
-//    if (numsSize < 2) {
-//        return;
-//    }
-//    int flag = *nums;
-//    int left = 0;
-//    int right = numsSize - 1;
-//
-//    while (left <= right) {
-//        while (*(nums + left) < flag) {
-//            left++;
-//        }
-//        while (*(nums + right) > flag) {
-//            right--;
-//        }
-//        swap(nums + left, nums + right);
-//    }
-//    swap(nums, nums + left);
-//    recursion(nums, left);
-//    recursion(nums + left + 1, numsSize - 1 - right);
-//}
-//
-//int *sortArray(int *nums, int numsSize, int *returnSize) {
-//    recursion(nums, numsSize);
-//    int *retArray = (int *) malloc(numsSize * sizeof(int));
-//    for (int i = 0; i < numsSize; i++) {
-//        retArray[i] = *(nums + i);
-//    }
-//    *returnSize = numsSize;
-//    return retArray;
-//}
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void recursion(int *nums, int numsSize) {
+    if (numsSize < 2) {
+        return;
+    }
+    int flag = *nums;
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left < right) {
+        while (*(nums + right) >= flag && right > left) {  // cause we use the left one as the flag, so the right must first.
+            right--;
+        }
+        while (*(nums + left) <= flag && left < right) {
+            left++;
+        }
+        swap(nums + left, nums + right);
+    }
+    swap(nums, nums + left);
+    recursion(nums, left);
+    recursion(nums + left + 1, numsSize - 1 - right);
+}
+
+int *sortArray(int *nums, int numsSize, int *returnSize) {
+    recursion(nums, numsSize);
+    *returnSize = numsSize;
+    return nums;
+}
 
