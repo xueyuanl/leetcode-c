da92cfe6b98509ed33822607a2cd4f3c8719452d
pi <15186846+xueyuanl@users.noreply.github.com>
2019-09-14 08:27:29 +0100

    update

--- src/string/first_unique_character_in_a_string.c
+++ src/string/first_unique_character_in_a_string.c
@@ -1,12 +1,13 @@
 //
-// Created by hsuehyuan li on 2019-04-19.
+// Created by hsuehyuan li on 2019-09-14.
 //
+
 #include <stdlib.h>
-#include "common/array.h"
 #include <string.h>
+#include <stdbool.h>
+
 
 int firstUniqChar(char *s) {
-
     int alphabet[26] = {0};
     int len = strlen(s);
     for (int i = 0; i < len; ++i) {
@@ -17,10 +18,4 @@ int firstUniqChar(char *s) {
         if (alphabet[*(s + i) - 'a'] == 1) return i;
     }
     return -1;
-}
-
-int main(int argc, const char *argv[]) {
-    firstUniqChar("loveleetcode");
-
-
 }
